be0e1b8cbfa4c707389718885bdeba5eea591094
Nathan Overmann <novermann@prograde.com>
2017-03-07 09:54:31 -0500

    Adding Deal Banner Directive

---
+++ js/directives/dealbanner.js
@@ -0,0 +1,8 @@
+four51.app.directive('dealbanner', function() {
+	var obj = {
+		restrict: 'E',
+		templateUrl: 'partials/controls/dealBanner.html',
+		controller: 'DealBannerCtrl'
+	};
+	return obj;
+});
